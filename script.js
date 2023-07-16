31bbd6e3a25508b85146823ebe2e69bda1b035a4
Iakapro <michaelandbrother@gmail.com>
2023-07-16 00:17:35 -0500

    mobilefix
    test if this bricks

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ const skipButtonCooldown = 600; // Cooldown duration in milliseconds
   
   function createYouTubePlayer() {
     player = new YT.Player('player', {
-      height: '300',
-      width: '300',
+      height: '0',
+      width: '0',
       videoId: playlist[shuffledIndices[currentIndex]].id,
       playerVars: {
         'controls': 0,
