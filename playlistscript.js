3a97488ca7145bd9bc40c070d6240425b0cc99a4
fakerincs <142262169+fakerincs@users.noreply.github.com>
2023-09-08 13:23:59 -0500

    Update playlistscript.js

--- playlistscript.js
+++ playlistscript.js
@@ -84,6 +84,7 @@ function createPlayer() {
   const indexSlider = document.getElementById('indexSlider');
   indexSlider.addEventListener('input', () => {
     playNextSong(indexSlider.value);
+    setTimeout(playNextSong(), 120)
   });
 }
 
