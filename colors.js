f2010298c8721f8897d72f63c2870110b73809ef
fakerincs <142262169+fakerincs@users.noreply.github.com>
2023-12-13 08:15:35 -0600

    Update colors.js

--- colors.js
+++ colors.js
@@ -1,35 +1,27 @@
-function resetColor(){
-  document.documentElement.style.setProperty("--main-color", "#a30000");
-  document.documentElement.style.setProperty("--played-color", "#c60035");
-  document.documentElement.style.setProperty("--background-color", "#ffffff");
-  document.documentElement.style.setProperty("--secondary-color", "#fec8cd");
-  document.documentElement.style.setProperty("--third-color", "#fec8cd");
-}
+
 function change(event) {
-  document.documentElement.style.setProperty(event.currentTarget.color, event.target.value);
-  event.currentTarget.saved = false;
+  document.documentElement.style.setProperty(event.target.color, event.target.value);
+  event.target.saved = false;
 }
 function save(event) {
-  event.currentTarget.saved = true;
-  document.documentElement.style.setProperty(event.currentTarget.color, event.target.value);
-  localStorage.setItem(event.currentTarget.id, event.target.value);
+  event.target.saved = true;
+  document.documentElement.style.setProperty(event.target.color, event.target.value);
+  localStorage.setItem(event.target.id, event.target.value);
 }
 function close(){
-  if (event.currentTarget.saved){
-    event.currentTarget.defaultColor= getComputedStyle(document.body).getPropertyValue(event.currentTarget.color);
+  if (event.target.saved){
+    event.target.defaultColor= getComputedStyle(document.body).getPropertyValue(event.target.color);
     return;
   }
-  document.documentElement.style.setProperty(event.currentTarget.color, event.currentTarget.defaultColor);
+  document.documentElement.style.setProperty(event.target.color, event.target.defaultColor);
 }
 
 function picker(id, color){
-  if (typeof(Storage) !== "undefined") {
-    if (localStorage.getItem(id)==null){
-      localStorage.setItem(id, getComputedStyle(document.body).getPropertyValue(color));
-    }
-    else{  
-      document.documentElement.style.setProperty(color, localStorage.getItem(id));
-    }
+  if (localStorage.getItem(id)==null){
+    localStorage.setItem(id, getComputedStyle(document.body).getPropertyValue(color));
+  }
+  else{  
+    document.documentElement.style.setProperty(color, localStorage.getItem(id));
   }
   var mPicker = document.getElementById(id);
   mPicker.defaultColor= getComputedStyle(document.body).getPropertyValue(color);
@@ -42,8 +34,10 @@ function picker(id, color){
   mPicker.addEventListener("change", save);
   mPicker.addEventListener("close", close);
 }
-picker("mainColor", "--main-color");
-picker("playedColor", "--played-color");
-picker("backgroundColor", "--background-color");
-picker("secondaryColor", "--secondary-color");
-picker("thirdColor", "--third-color");
+if (typeof(Storage) !== "undefined") {
+  picker("mainColor", "--main-color");
+  picker("playedColor", "--played-color");
+  picker("backgroundColor", "--background-color");
+  picker("secondaryColor", "--secondary-color");
+  picker("thirdColor", "--third-color");
+}
