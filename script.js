5d6e2e6f6278c24411dd4f60df257c8a0e6575df
fakerincs <142262169+fakerincs@users.noreply.github.com>
2023-12-13 14:24:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -645,55 +645,54 @@ navigator.mediaSession.setActionHandler('nexttrack', () => playNextSong());
 
 shuffleIndices();
 generateUpcoming();
-let myPromise = new Promise(function(myResolve, myReject)){
-  var loaded = 0;
-  var link = document.createElement('link');
-  link.onload = function () {
-    console.log("1");
-    loaded++;
-  }
-  link.href = "https://vjs.zencdn.net/8.3.0/video-js.min.css";
-  link.rel = "stylesheet";
-  document.getElementsByTagName("head")[0].appendChild(link);
+// let myPromise = new Promise(function(myResolve, myReject)){
+//   var loaded = 0;
+//   var link = document.createElement('link');
+//   link.onload = function () {
+//     console.log("1");
+//     loaded++;
+//   }
+//   link.href = "https://vjs.zencdn.net/8.3.0/video-js.min.css";
+//   link.rel = "stylesheet";
+//   document.getElementsByTagName("head")[0].appendChild(link);
   
   
-  var script = document.createElement('script');
-  script.onload = function () {
-    console.log("2");
-    loaded++;
-  };
-  script.src = "https://vjs.zencdn.net/8.3.0/video.min.js";
-  document.body.appendChild(script);
+//   var script = document.createElement('script');
+//   script.onload = function () {
+//     console.log("2");
+//     loaded++;
+//   };
+//   script.src = "https://vjs.zencdn.net/8.3.0/video.min.js";
+//   document.body.appendChild(script);
   
   
   
   
+//   var scriptyt = document.createElement('script');
+//       scriptyt.onload = function () {
+//         myResolve("OK");
+//         loaded++;
+//     };
+//   scriptyt.src = "https://cdnjs.cloudflare.com/ajax/libs/videojs-youtube/3.0.1/Youtube.min.js";
+//   scriptyt.crossOrigin="anonymous";
+//   scriptyt.referrerPolicy = "no-referrer";
+//   scriptyt.setAttribute("crossorigin", "anonymous");
+//   scriptyt.setAttribute("referrerpolicy", "no-referrer");
   
-  var scriptyt = document.createElement('script');
-      scriptyt.onload = function () {
-        myResolve("OK");
-        loaded++;
-    };
-  scriptyt.src = "https://cdnjs.cloudflare.com/ajax/libs/videojs-youtube/3.0.1/Youtube.min.js";
-  scriptyt.crossOrigin="anonymous";
-  scriptyt.referrerPolicy = "no-referrer";
-  scriptyt.setAttribute("crossorigin", "anonymous");
-  scriptyt.setAttribute("referrerpolicy", "no-referrer");
-  
-  scriptyt.integrity = "sha512-W11MwS4c4ZsiIeMchCx7OtlWx7yQccsPpw2dE94AEsZOa3pmSMbrcFjJ2J7qBSHjnYKe6yRuROHCUHsx8mGmhA==";
-  console.log(scriptyt);
-  document.body.appendChild(scriptyt);
-}
+//   scriptyt.integrity = "sha512-W11MwS4c4ZsiIeMchCx7OtlWx7yQccsPpw2dE94AEsZOa3pmSMbrcFjJ2J7qBSHjnYKe6yRuROHCUHsx8mGmhA==";
+//   console.log(scriptyt);
+//   document.body.appendChild(scriptyt);
+// }
 
-function resolution(){
-  // playerl = new Audio('songs/Recording.mp3');
-  // playerl.pause();
-  // playerl.addEventListener('ended', function(){
-  //   playNextSong();
-  // });
+// function resolution(){
+playerl = new Audio('songs/Recording.mp3');
+playerl.pause();
+playerl.addEventListener('ended', function(){
+playNextSong();
+//   // });
   createPlayer();
-}
-myPromise.then(
-  function() {resolution();},
-);
+// }
+// myPromise.then(
+//   function() {resolution();},
+// );
 
