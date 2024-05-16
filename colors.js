e5bd7e9656d7083fe1cfc2dac20f3055bc54db57
fakerincs <142262169+fakerincs@users.noreply.github.com>
2024-05-16 08:39:40 -0500

    Update colors.js

--- colors.js
+++ colors.js
@@ -43,7 +43,7 @@ if (typeof(Storage) !== "undefined") {
   picker("upcomingColor", "--upcoming-color");
   picker("songsColor", "--songs-color");
   picker("borderColor", "--border-color");
-  document.documentElement.style.setProperty("borderw", localStorage.getItem("borderw") + "px");
+  document.documentElement.style.setProperty("--borderw", localStorage.getItem("borderw") + "px");
   document.getElementById("borderw").value = localStorage.getItem("borderw");
 function resetcolors(){
   localStorage.removeItem("mainColor");
